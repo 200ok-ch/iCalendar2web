777af6472db2a07ebe2177a5b81fee6985201b90
Alain M. Lafon <alain@200ok.ch>
2019-08-10 10:40:28 +0200

    Linting with standard rubocop cops

--- meetup_cal.rb
+++ meetup_cal.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rubygems'
 require 'bundler'
 Bundler.require
@@ -13,10 +15,10 @@ require 'active_support/all'
 MEETUP_API_KEY = ENV['MEETUP_API_KEY']
 get '/meetup/:meetup_group_url' do
   headers 'Access-Control-Allow-Origin' => '*'
-  url = "http://api.meetup.com/2/events?offset=0&format=json&limited_events=False" +
-    "&group_urlname=#{params['meetup_group_url']}" +
-    "&photo-host=public&page=200&fields=&order=time&desc=false&status=upcoming&sign=true" +
-    "&key=#{MEETUP_API_KEY}"
+  url = 'http://api.meetup.com/2/events?offset=0&format=json&limited_events=False' \
+        "&group_urlname=#{params['meetup_group_url']}" \
+        '&photo-host=public&page=200&fields=&order=time&desc=false&status=upcoming&sign=true' \
+        "&key=#{MEETUP_API_KEY}"
 
   res = Net::HTTP.get(URI(url))
 
@@ -37,5 +39,5 @@ get '/meetup/:meetup_group_url' do
 end
 
 get '/' do
-  "Please visit /meetup/YourMeetupGroupURL"
+  'Please visit /meetup/YourMeetupGroupURL'
 end
